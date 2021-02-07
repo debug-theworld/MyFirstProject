5ea581a071480a891a4b979b9828bd54616b7b6f
hihi <ghw97283460@gmail.com>
2021-02-07 17:12:45 +0800

    Added a hello world program in my first project

---
+++ hello world.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	cout << "This is the first project in my first project repo" << endl;
+
+
+	return 0;
+}
